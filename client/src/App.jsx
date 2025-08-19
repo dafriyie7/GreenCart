9972b48d87de420d071230c4481c1cf4c82673ee
Daniel Afriyie <dafriyie7@gmail.com>
2025-08-19 14:17:53 +0000

    complete 1/3

---
+++ client/src/App.jsx
@@ -0,0 +1,36 @@
+import { Route, Routes, useLocation } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
+import { useAppContext } from "./context/AppContext";
+import Login from "./components/Login";
+import AllProducts from "./pages/AllProducts";
+
+
+function App() {
+	const isSellerPath = useLocation().pathname.includes("seller")
+	const { showuserLogin } = useAppContext();
+
+  return (
+		<div>
+			{!isSellerPath && <Navbar />}
+			{showuserLogin && <Login />}
+
+			<Toaster />
+			<div
+				className={`${
+					isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"
+				}`}
+			>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/products" element={<AllProducts />} />
+				</Routes>
+			</div>
+			{!isSellerPath && <Footer />}
+		</div>
+  );
+}
+
+export default App

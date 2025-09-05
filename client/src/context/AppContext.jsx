3f41ebdbb3b101e2eb9f46d8a862738bf745528c
Daniel Afriyie <dafriyie7@gmail.com>
2025-09-05 10:33:55 +0000

    vercel config

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { dummyProducts } from "../assets/assets";
 import toast from "react-hot-toast";
+import axios from "axios"
 
 export const AppContext = createContext();
 
@@ -102,6 +103,7 @@ export const AppContextProvider = ({ children }) => {
 		setSearchQuery,
 		getCartAmount,
 		getCartCount,
+		axios
 	};
 
 	return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
